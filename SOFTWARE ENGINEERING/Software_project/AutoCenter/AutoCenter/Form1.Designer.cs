f828f0c42399f4f9861653092c0b61e0ffc8566b
abdarum <kornelstefanczyk@wp.pl>
2020-11-25 14:37:12 +0200

    Added First container

--- SOFTWARE ENGINEERING/Software_project/AutoCenter/AutoCenter/Form1.Designer.cs
+++ SOFTWARE ENGINEERING/Software_project/AutoCenter/AutoCenter/Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBoxStereoSystem = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBoxStereoSystem);
+            this.groupBox1.Location = new System.Drawing.Point(24, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Accessories";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 89);
+            this.checkBox3.Name = "checkBoxComputerNavigation";
+            this.checkBox3.Size = new System.Drawing.Size(101, 24);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Computer Navigation";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 58);
+            this.checkBox2.Name = "checkBoxLeatherInterior";
+            this.checkBox2.Size = new System.Drawing.Size(101, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Leather Interior";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxStereoSystem
+            // 
+            this.checkBoxStereoSystem.AutoSize = true;
+            this.checkBoxStereoSystem.Location = new System.Drawing.Point(6, 26);
+            this.checkBoxStereoSystem.Name = "checkBoxStereoSystem";
+            this.checkBoxStereoSystem.Size = new System.Drawing.Size(125, 24);
+            this.checkBoxStereoSystem.TabIndex = 0;
+            this.checkBoxStereoSystem.Text = "Stereo System";
+            this.checkBoxStereoSystem.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Auto Center";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBoxStereoSystem;
     }
 }
 
--- SOFTWARE ENGINEERING/Software_project/AutoCenter/AutoCenter/Form1.cs
+++ SOFTWARE ENGINEERING/Software_project/AutoCenter/AutoCenter/Form1.cs
@@ -17,5 +17,10 @@ namespace AutoCenter
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
