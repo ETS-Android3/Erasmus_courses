276c550fe138680e7a7ac254e3105be2e0041525
abdarum <kornelstefanczyk@wp.pl>
2020-12-03 17:17:42 +0200

    task 2 completed

--- COMPUTER GRAPHICS AND VISUALIZATION/Projects/Lab03_exam/task2.cpp
+++ COMPUTER GRAPHICS AND VISUALIZATION/Projects/Lab03_exam/task2.cpp
@@ -9,26 +9,60 @@
 
 // 7. THE ECONOMIC GRAPHICS, Histograms and circle diagrams For example: Input data: 3 real data which correspond with financial result of one firm. The program has to obtain graphical presentation of financial result a firm for last 3 months like vertical histograms in gr. window: (x0,y0)=(30,420), Ds=30, Dc=40, Py=300.
 
-#define POINTS_NUM 3
-#define AXIS_LABELS 5
-
 using namespace std;
 int main(){
     // chart root point (left-down corner of the window)
-    int x0, y0, dx, dy;
-    x0 = 30;    y0 = 420;
-    // size of the chart axis
-    dx = 300;   dy = 300;
-    float x[POINTS_NUM], y[POINTS_NUM], maxX, minX, maxY, minY;
+    int x0 = 30, y0 = 420;
+    float a[] = {10352.12, 4002.53, 26142.86};
+    char labels[][20]={"October", "November", "December"};
+    int n=sizeof(a)/sizeof(a[0]);
 
-    initwindow(dx+100, y0+50, "Example");
+    int Px=300,Py=300,D=100,Ds=30,Dc=40 ;
+    int i,x1,y1,x2,y2;
+    int winwidth=Px+200,winheight=Py+200;
+    initwindow(winwidth, winheight, "Example");
+
     
-    // assign values
-    for(int i=0; i<POINTS_NUM; i++){
-        x[i] = i +1;
-        y[i] = x[i]*x[i] + 5.0*sin(x[i]);
-        // std::cout << "i: " << i << " x: " << x[i] << "\t y: " << y[i] << std::endl;
-    } 
+    float amin = a[0];
+    float amax = a[0];
+    for(int i = 1; i< n; i++) {
+        if(a[i] < amin) amin = a[i];
+        if(a[i] > amax) amax = a[i];
+    }
+
+    if (amin>0) amin=0;
+    if (amax<0) amax=0;
+    float s = (amax - amin)/Py;
+
+    int y0n=y0+amin/s;
+
+    line(x0,y0n,x0+Px,y0n);
+    line(x0,y0,x0,y0-Py);
+    int I = Py/D; 
+    char text[10];
+
+    for(i=0; i<=I; i++)
+    {
+        line(x0, y0-D*i, x0-3, y0-D*i);
+        sprintf(text, "%.2f",amin + i*D*s);
+        settextjustify(2,1);
+        outtextxy(x0-10,y0-D*i+5, text); 
+    }
+    for(i=1; i<=n; i++)
+    {
+        settextjustify(1,2);
+        outtextxy(x0+i*(Ds+Dc)-Ds/2,y0+5, labels[i-1]); 
+    }
+    for(i=1; i<n+1; i++)
+    {
+        x1=x0+i*(Ds+Dc)-Ds;
+        y1=y0n-(a[i-1])/s;
+        x2=x0+i*(Ds+Dc);
+        y2=y0n;
+        setfillstyle(1, i+1);
+        bar(x1,y1,x2,y2);
+    }
+
 
     getch();
     return 0;
