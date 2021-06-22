9305f01843575b85f3f8584a1f891198728ac80d
abdarum <kornelstefanczyk@wp.pl>
2021-06-22 15:27:06 +0300

    Working progress bar

--- Programming_for_Mobile_Internet_Devices/Task_4/app/src/main/java/com/example/task_4/MainActivity.java
+++ Programming_for_Mobile_Internet_Devices/Task_4/app/src/main/java/com/example/task_4/MainActivity.java
@@ -8,12 +8,19 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int progressBarMax = 100;
+    private static final int progressBarStepTimeSleep = 20;
+
+    private static int progress;
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,35 +29,58 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        progress = 0;
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        progressBar.setMax(progressBarMax);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        handler.postAtFrontOfQueue(new Runnable() {
+                            @Override
+                            public void run() {
+                                progress = 0;
+                                progressStatus = 0;
+                                progressBar.setProgress(progress);
+                                progressBar.setVisibility(View.VISIBLE);
+                            }
+                        });
+                        while (progressStatus < progressBarMax) {
+                            progressStatus = doSomeWork();
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressStatus);
+                                }
+                            });
+                        }
+                        handler.post(new Runnable() {
+                            public void run() {
+                                progressBar.setVisibility(View.GONE);
+                                Snackbar.make(view, "Done", Snackbar.LENGTH_LONG)
+                                        .setAction("Action", null).show();
+                            }
+                        });
+                    }
+                    private int doSomeWork() {
+                        try {
+                            // ---simulate doing some work---
+                            Thread.sleep(progressBarStepTimeSleep);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return ++progress;
+                    }
+                }).start();
+
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
